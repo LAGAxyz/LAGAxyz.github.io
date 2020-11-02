6c0a1afdf575de37a4be003db56e4f146c063b15
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-11-01 22:43:56 -0500

    Estilos Ness.css y Typed.JS agregados

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const options = {
+  strings: [
+      'My name is Luis Alberto Gutierrez Anicama',
+      'You can call me LAGA',
+      'This is Luis Alberto Gutierrez Anicama'
+    ],
+  typeSpeed: 60,
+  backSpeed: 45,
+  backDelay: 800,
+  loop: true,
+};
+
+const typed = new Typed('.typedJS', options);
