8a8868f779306d92e48b655ae00c8f7368d8a229
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-11-01 23:14:48 -0500

    Animación de la barra de progreso en JS

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 
+const webSite = document.getElementById("webSSite");
+const perfilGH = document.getElementById("perfilGH");
+
+const iconFB = document.getElementById("iconFB");
+const iconIG = document.getElementById("iconIG");
+const iconLI = document.getElementById("iconLI");
+const iconYT = document.getElementById("iconYT");
+const iconWA = document.getElementById("iconWA");
+const iconGH = document.getElementById("iconGH");
+
+const bar = document.getElementById("bar");
+
 const options = {
   strings: [
       'My name is Luis Alberto Gutierrez Anicama',
@@ -12,3 +24,7 @@ const options = {
 };
 
 const typed = new Typed('.typedJS', options);
+
+setInterval(() => {
+  bar.value === 100 ? bar.value = 0 : bar.value += 1;
+}, 1000);
