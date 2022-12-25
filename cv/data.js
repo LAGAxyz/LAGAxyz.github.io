20bb8e9ea46eea3f19dec1ae18ad175e81966ab2
LAGAxyz <gutierrezanicamaluisalberto@hotmail.com>
2022-12-25 09:48:42 -0500

    Página de CV agregada (primera versión)

---
+++ cv/data.js
@@ -0,0 +1,132 @@
+
+export const certificaciones = [
+    {
+        lugar: "Instituto Científico del Pacífico",
+        valor: "Excel Financiero",
+        fecha: "2020 julio",
+        imagen: "../assets/img/certificaciones/icip-excel-financiero.jpg"
+    },
+    {
+        lugar: "Sistemas Integrales TI (SITI Perú)",
+        valor: "Análisis de datos con Power BI y Excel",
+        fecha: "2019 diciembre",
+        imagen: "../assets/img/certificaciones/taller-siti-peru.jpg"
+    },
+    {
+        lugar: "Centro Informático 'Ingenieros UNI'",
+        valor: "Especialista en Excel",
+        fecha: "2017 octubre",
+        imagen: "../assets/img/certificaciones/ing-uni-especialista-excel.jpg"
+    },
+    {
+        lugar: "Centro Informático 'Ingenieros UNI'",
+        valor: "Diseño Gráfico",
+        fecha: "2017 agosto",
+        imagen: "../assets/img/certificaciones/ing-uni-disenio-grafico.jpg"
+    },
+    {
+        lugar: "WorkShop CETI Perú",
+        valor: "Ciberseguridad Organizacional",
+        fecha: "2016 septiembre",
+        imagen: "../assets/img/certificaciones/taller-ceti-peru.jpg"
+    },
+    {
+        lugar: "Instituto de Educación Superior Tecnológico de Huaycán",
+        valor: "Recursos Digitales en los Entornos Virtuales de la Informática",
+        fecha: "2015 noviembre",
+        imagen: "../assets/img/certificaciones/seminario-iesth-2015-ii.jpg"
+    },
+    {
+        lugar: "Instituto de Educación Superior Tecnológico de Huaycán",
+        valor: "Uso de las TIC en la Modernización de Organizaciones Públicas y Privadas",
+        fecha: "2015 noviembre",
+        imagen: "../assets/img/certificaciones/seminario-iesth-2015-i.jpg"
+    },
+    {
+        lugar: "Instituto de Educación Tecnológico Privado CATÓLICA",
+        valor: "Ensamblaje y Reparación de PC'S",
+        fecha: "2015 octubre",
+        imagen: "../assets/img/certificaciones/iestp-catolica.jpg"
+    },
+    {
+        lugar: "Instituo de Educación Superior Tecnológico Privado UNICENTER",
+        valor: "Especialista en Instalación de Cabinas de Internet",
+        fecha: "2015 octubre",
+        imagen: "../assets/img/certificaciones/iestp-unicenter-4.jpg"
+    },
+    {
+        lugar: "Instituo de Educación Superior Tecnológico Privado UNICENTER",
+        valor: "Especialista en Software",
+        fecha: "2015 julio",
+        imagen: "../assets/img/certificaciones/iestp-unicenter-3.jpg"
+    },
+    {
+        lugar: "Instituo de Educación Superior Tecnológico Privado UNICENTER",
+        valor: "Especialista en Reparación de PC'S",
+        fecha: "2015 junio",
+        imagen: "../assets/img/certificaciones/iestp-unicenter-2.jpg"
+    },
+    {
+        lugar: "Universidad Nacional de Ingeniería",
+        valor: "Las TIC un Aliado Fundamental para el Emprendimiento",
+        fecha: "2015 mayo",
+        imagen: "../assets/img/certificaciones/taller-tics-uni.jpg"
+    },
+    {
+        lugar: "Microsoft Parnet Network",
+        valor: "Tecnología de la Información y Comunicación: Aplicaciones y Novedades",
+        fecha: "2015 abril",
+        imagen: "../assets/img/certificaciones/taller-tics-microsoft.jpg"
+    },
+    {
+        lugar: "Instituto de Educación Superior Tecnológico Privado UNICENTER",
+        valor: "Especialista Técnico en Soporte Hardware de PC's",
+        fecha: "2015 marzo",
+        imagen: "../assets/img/certificaciones/iestp-unicenter-1.jpg"
+    },
+    {
+        lugar: "Instituto de Educación Superior Tecnológico Privado UNICENTER",
+        valor: "Aplicaciones en Microsoft Project",
+        fecha: "2015 febrero",
+        imagen: "../assets/img/certificaciones/taller-unicenter-ms-project.jpg"
+    },
+    {
+        lugar: "Instituto de Educación Superior Tecnológico de Huaycán",
+        valor: "Gestión e Implementación de las Tics y Páginas Web",
+        fecha: "2014 octubre",
+        imagen: "../assets/img/certificaciones/seminario-iesth-2014.jpg"
+    }
+]
+
+export const experiencias = [
+    {
+        lugar: "Instituto de Educación Superior Tecnológico Privado COMPUTRON",
+        cargo: "Docente",
+        fecha: "2019 agosto - 2020 marzo",
+        constancia: "https://via.placeholder.com/300x500?text=Constancia+en+espera"
+    },
+    {
+        lugar: "Centro de Educación Técnico Productivo de Huaycán",
+        cargo: "Practicante Informático",
+        fecha: "2019 abril - 2019 junio",
+        constancia: "../assets/img/certificaciones/pp-cetpro-huaycan.jpg"
+    },
+    {
+        lugar: "Instituto de Educación Superior Tecnológico Privado AMAUTA",
+        cargo: "Docente",
+        fecha: "2017 febrero - 2018 julio",
+        constancia: "../assets/img/certificaciones/trabajo-iestp-amauta.jpg"
+    },
+    {
+        lugar: "Centro de Educación Técnico Productivo de Huaycán",
+        cargo: "Practicante Informático",
+        fecha: "2015 junio - 2016 enero",
+        constancia: "../assets/img/certificaciones/ppp-cetpro-huaycan.jpg"
+    },
+    {
+        lugar: "Instituto de Educación Superior Tecnológico de Huaycán",
+        cargo: "Practicante Informático",
+        fecha: "2014 octubre - 2014 diciembre",
+        constancia: "../assets/img/certificaciones/ppp-iesth.jpg"
+    }
+]
---
+++ cv/main.js
@@ -0,0 +1,99 @@
+import {certificaciones, experiencias} from "./data.js";
+
+const modal = document.getElementById("modal");
+const imgModal = document.getElementById("imgModal");
+const btnModal = document.getElementById("btnModal");
+
+document.getElementById("btn01").onclick = ()=> {
+    modal.style.display = "flex";
+    imgModal.src = "../assets/img/certificaciones/iestp-sise-egresado-eti.jpg"
+}
+
+document.getElementById("btnEs").onclick = ()=> {
+    modal.style.display = "flex";
+    imgModal.src = "../assets/img/certificaciones/idioma-castellano-nativo.jpg";
+}
+
+document.getElementById("btnEn").onclick = ()=> {
+    modal.style.display = "flex";
+    imgModal.src = "../assets/img/certificaciones/idioma-ingles-basico.jpg";
+}
+
+const listarCertificaciones = ()=> {
+    const contenidoCertificaciones = document.getElementById("contenidoCertificaciones");
+    contenidoCertificaciones.innerHTML = "";
+
+    certificaciones.forEach((certificacion)=>{
+        let divCertificacion = document.createElement("div");
+            divCertificacion.classList.add("certificacion");
+        let divCertificado = document.createElement("div");
+            divCertificado.classList.add("certificado");
+        let btnCertificado = document.createElement("button");
+            btnCertificado.type = "button";
+            btnCertificado.innerText = "Ver Certificado";
+            btnCertificado.classList.add("btnVerCertificado");
+        let separador = document.createElement("hr");
+
+        btnCertificado.onclick = ()=> {
+            modal.style.display = "flex";
+            imgModal.src = certificacion.imagen;
+        }
+
+        let html = `<div class="informacion">
+                        <p><i class="fa fa-map-marker"></i><span>${certificacion.lugar}</span></p>
+                        <p><i class="fa fa-file-text"></i><span>${certificacion.valor}</span></p>
+                        <p><i class="fa fa-calendar"></i><span>${certificacion.fecha}</span></p>
+                    </div>`
+
+    divCertificado.appendChild(btnCertificado);
+
+    divCertificacion.innerHTML = html;
+    divCertificacion.appendChild(divCertificado);
+    
+    contenidoCertificaciones.appendChild(separador);
+    contenidoCertificaciones.appendChild(divCertificacion);
+    })
+}
+
+const listarExperiencia = ()=> {
+    const contenidoExperiencia = document.getElementById("contenidoExperiencia");
+    contenidoExperiencia.innerHTML = "";
+
+    experiencias.forEach((experiencia)=>{
+        let divExperiencia = document.createElement("div");
+            divExperiencia.classList.add("experiencia");
+        let divCertificado = document.createElement("div");
+            divCertificado.classList.add("certificado");
+        let btnCertificado = document.createElement("button");
+            btnCertificado.type = "button";
+            btnCertificado.innerText = "Ver Constancia";
+            btnCertificado.classList.add("btnVerCertificado");
+        let separador = document.createElement("hr");
+
+        btnCertificado.onclick = ()=> {
+            modal.style.display = "flex";
+            imgModal.src = experiencia.constancia;
+        }
+
+        let html = `<div class="informacion">
+                        <p><i class="fa fa-home"></i><span>${experiencia.lugar}</span></p>
+                        <p><i class="fa fa-id-card"></i><span>${experiencia.cargo}</span></p>
+                        <p><i class="fa fa-calendar"></i><span>${experiencia.fecha}</span></p>
+                    </div>`
+
+    divCertificado.appendChild(btnCertificado);
+
+    divExperiencia.innerHTML = html;
+    divExperiencia.appendChild(divCertificado);
+    
+    contenidoExperiencia.appendChild(separador);
+    contenidoExperiencia.appendChild(divExperiencia);
+    })
+}
+
+listarCertificaciones();
+listarExperiencia();
+
+btnModal.onclick = ()=> {
+    modal.style.display = "none";
+}
